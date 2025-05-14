1d37ed241465ce568c8d4367ea226ba9b67a05b9
25LP7341 25LP7341 <25LP7341@admins-iMac.local>
2025-05-14 12:38:53 +0800

    temp

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,11 +1,11 @@
 import { HeartIcon, HomeIcon, MapPinIcon, UserSearchIcon } from "lucide-react";
-export const Day = () => {
+export const Day = ({temp}) => {
   return (
     <div className="border border-gray-300 shadow-xl h-fit w-fit px-20 rounded-4xl">
       <p className="text-gray-500 pt-10">May13, 2025</p>
       <h1 className="text-5xl font-bold pt-3">Ulaanbaatar</h1>
       <img className="h-80 w-80 mt-10" src="sun.png" />
-      <h2 className="text-9xl pt-24 text-black">26°</h2>
+      <h2 className="text-9xl pt-24 text-black">{temp}</h2>
       <p className="text-[#FF8E27] text-2xl font-bold p-9 ">Bright</p>
       <div className="flex justify-between pb-10">
         <HomeIcon />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,47 @@
 import { Day } from "@/components/Day";
 import { Night } from "@/components/Night";
+import { useState } from "react";
 
 export default function Home() {
+  const [weather, setWeather] = useState({});
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=35.652832&lon=139.839478&appid=${process.env.NEXT_PUBLIC_WHEATHER_API_KEY}&units=metric`;
+  const cityUrl = `https://api.api-ninjas.com/v1/city?name=Tokyo`;
+  const getCity = async () => {
+    try {
+      const response = await fetch(cityUrl, {
+        headers: {
+          "X-Api-Key": process.env.NEXT_PUBLIC_CITY_API_KEY,
+        },
+      });
+      const data = await response.json();
+      return data;
+      // console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getWeather = async () => {
+    try {
+      const cityLocation = await getCity();
+
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${cityLocation[0].latitude}&lon=${cityLocation[0].longitude}&units=metricc&appid=${process.env.NEXT_PUBLIC_WHEATHER_API_KEY}`
+      );
+      const data = await response.json();
+      // console.log(data);
+
+      setWeather(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="flex justify-center pt-20 gap-10 ">
-      <Day />
+      <button onClick={getWeather}>get</button>
+      <p>temp:{weather?.main?.temp}</p>
+      <Day temp={weather?.main?.temp} />
       <Night />
-      
     </div>
   );
 }
